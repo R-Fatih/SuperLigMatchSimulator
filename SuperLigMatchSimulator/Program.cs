fcefc8330c7bff4f1f95d23f0b948ab31b59b8f6
RFatih <39864827+R-Fatih@users.noreply.github.com>
2025-01-24 15:08:47 +0300

    up

--- SuperLigMatchSimulator/Program.cs
+++ SuperLigMatchSimulator/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Match}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
